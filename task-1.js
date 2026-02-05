c237d16d9280efa028ee71d147e71447a637f3fd
PhantomOblivion <smasif404@gmail.com>
2026-02-05 11:10:40 +0600

    Solved the array looping related task-1.

--- task-1.js
+++ task-1.js
@@ -1,3 +1,25 @@
 // Task-1: Write a JavaScript code to reverse the array colors without using the reverse method.
 
-const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+let reverse = []; //I have created and empty array and named it reverse. The reverse order of colors will be store here.
+const newColors = colors.length - 1; //total number 5 - 1; cause, array indices start from 0
+
+//Using while Loop
+/*
+let i = newColors;
+
+while(i>=0){
+    reverse.push(colors[i]);
+    i--;
+}
+console.log(reverse);
+*/
+
+
+//Using for Loop
+
+for (let i = newColors; i >= 0; i--){
+    reverse.push(colors[i]);
+}
+console.log(reverse);
